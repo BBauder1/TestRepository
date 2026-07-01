686543982a792818fa909f9cab83b9eb502696ef
Brandon Bauder <45412061+BBauder1@users.noreply.github.com>
2026-07-01 14:01:40 -0700

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,20 @@
+
+// Online IDE - Code Editor, Compiler, Interpreter
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Bob Wilkson" << endl << "45";
+    cout << " \ntest\n";
+    
+    int num = 0;
+    
+    cin >> num;
+    
+    cout << "This is your input: " << num;
+    
+    return 0;
+}
